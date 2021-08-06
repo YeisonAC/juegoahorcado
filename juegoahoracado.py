8d14e4ae75fde2cef31a309bc286072ea11ef0cf
Yeison Arroyave <Yeisonarroyave.ca@gmail.com>
2021-08-05 23:08:19 -0500

    se organizo la letra

--- juegoahoracado.py
+++ juegoahoracado.py
@@ -3,23 +3,23 @@ print("adivina la palabra!")
 letra = (input("Ingresa una letra: "))
 print(letra)
 
-#os.system("cls")
     
+
+
 
 def read():
     data = []
-    with open (("./DATA/data.txt"), "r") as f:
+    with open (("./DATA/data.txt"), "r", encoding="UTF-8") as f:
         for i, f in enumerate(f):
             print(i, f)
-
-
-#correr ciclo
-#agregar = [i for i in range(f) ]
-
-
+           
+    
+    
+#os.system("cls")
 
 def run():
-    pass
+    import random
+    agregar = []
 
 if __name__ =="__main__":
     read()
